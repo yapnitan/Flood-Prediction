7440561cea0ec4f964a1f2c31f97123be9cc21fd
TanYapNi <yapnitan@gmail.com>
2026-07-12 20:55:43 +0800

    create class for different user

---
+++ lib/user_management/models/account.dart
@@ -0,0 +1,15 @@
+class Account {
+  String id;
+  String name;
+  String email;
+  String password;
+  String role;
+
+  Account({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.role,
+  });
+}
---
+++ lib/user_management/service/auth_service.dart
@@ -0,0 +1,5 @@
+import '../models/account.dart';
+
+class AuthService {
+
+}
