ddac47b244ed5396a5c1bbd3dc2447f70bf9d77e
TanYapNi <yapnitan@gmail.com>
2026-07-12 18:05:26 +0800

    Create user_model.dart
    create user model

---
+++ lib/user_management/models/user_model.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  String id;
+  String name;
+  String email;
+  String password;
+  String role;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.role,
+  });
+}
